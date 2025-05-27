3169c54700279a921bb6a908f91981bc294c9e65
Satyam6767 <satyamkumar6767@gmail.com>
2025-05-27 16:24:33 +0530

    initial commit

--- hirehub-backend/server.js
+++ hirehub-backend/server.js
@@ -27,6 +27,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applicationRoutes);
 
+
+app.get('/', (req, res) => {
+  res.send('Hello! Backend is running.');
+});
+
 // ✅ Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
